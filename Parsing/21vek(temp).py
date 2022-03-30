bbb575e007d62369407a5ed6a083ffe7bdbf5f40
серго <p.u.n.i.s.h.e.r@mail.ru>
2022-03-30 21:49:53 +0300

    init

---
+++ Parsing/21vek(temp).py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+a=set()
+r=requests.get("https://www.21vek.by/")
+soup = BeautifulSoup(r.text,'lxml')
+soup = soup.findAll('span',class_="styles-moduleQc4Xjg5U9Q")
+for i in soup:
+    a.add(i.get_text())
+
+for j in sorted(a):
+    print(j)
+
+
+
